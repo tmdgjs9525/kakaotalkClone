4b4d4565c97d6cfce1805741f56a59019daf29c6
이광석 <kevin.lgs@aceenter.com>
2023-03-02 12:35:51 +0900

    - CheckBoxCircle Style 이관  - 불필요한 코드 정리

--- kakaotalkClone/kakaotalkClone/App.cs
+++ kakaotalkClone/kakaotalkClone/App.cs
@@ -1,22 +1,16 @@
 ﻿using kakaotalkClone.ViewModels;
 using kakaotalkClone.Views;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace kakaotalkClone
 {
-    public class Appd : Application
-    {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            LoginWindow login = new LoginWindow();
-            login.DataContext = new LoginViewModel();
-            login.ShowDialog();
-        }
-    }
+		public class App : Application
+		{
+				protected override void OnStartup(StartupEventArgs e)
+				{
+						base.OnStartup (e);
+						LoginWindow login = new LoginWindow ();
+						login.ShowDialog ();
+				}
+		}
 }
--- kakaotalkClone/kakaotalkClone/Views/MainWindow.xaml.cs
+++ kakaotalkClone/kakaotalkClone/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using kakaotalkClone.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,14 +15,15 @@ using System.Windows.Shapes;
 
 namespace kakaotalkClone.Views
 {
-    /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+		/// <summary>
+		/// MainWindow.xaml에 대한 상호 작용 논리
+		/// </summary>
+		public partial class MainWindow : Window
+		{
+				public MainWindow()
+				{
+						InitializeComponent ();
+						this.DataContext = new mainWindowViewModel ();
+				}
+		}
 }
